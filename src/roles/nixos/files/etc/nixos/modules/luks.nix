f711e4ea74258f76dd8723bdfb9a48a40b6ba437
Richard Slater <git@scetrov.live>
2025-02-23 16:14:11 +0000

    fix(): reenable boot

--- src/roles/nixos/files/etc/nixos/modules/luks.nix
+++ src/roles/nixos/files/etc/nixos/modules/luks.nix
@@ -15,7 +15,7 @@ in
   # This setting is usually set to true in configuration.nix
   # generated at installation time. So we force it to false
   # for now.
-  boot.loader.systemd-boot.enable = lib.mkForce false;
+  # boot.loader.systemd-boot.enable = lib.mkForce false;
 
   # boot.lanzaboote = {
   #   enable = true;
