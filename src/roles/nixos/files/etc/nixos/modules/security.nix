b1a965597ee61fdfcddb4ceb8e915aa795b2045b
Richard Slater <git@scetrov.live>
2025-02-23 23:11:43 +0000

    fix(): add udev rules for yubikey

--- src/roles/nixos/files/etc/nixos/modules/security.nix
+++ src/roles/nixos/files/etc/nixos/modules/security.nix
@@ -15,7 +15,7 @@
     };
   };
 
-  services.pcscd.enable = true;
+  services.pcscd.enable = false;
 
   security.pam.yubico = {
     enable = true;
@@ -23,4 +23,15 @@
     mode = "challenge-response";
     id = [ "11073070" ];
   };
+
+  services.udev.extraRules = ''
+    ACTION!="add|change", GOTO="yubikey_end"
+
+    # Udev rule for YubiKey 5 series (e.g. YubiKey 5 NFC)
+    ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", \
+        ENV{ID_SECURITY_TOKEN}="1"
+
+    LABEL="yubikey_end"
+  '';
+  
 }
