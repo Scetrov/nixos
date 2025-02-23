587f04b5a829af23f48a5660688c8acc2a4744e6
Richard Slater <git@scetrov.live>
2025-02-23 16:39:40 +0000

    feat(): add niv

--- src/roles/nixos/files/etc/nixos/modules/luks.nix
+++ src/roles/nixos/files/etc/nixos/modules/luks.nix
@@ -17,10 +17,10 @@ in
   # This setting is usually set to true in configuration.nix
   # generated at installation time. So we force it to false
   # for now.
-  boot.loader.systemd-boot.enable = lib.mkForce false;
+  # boot.loader.systemd-boot.enable = lib.mkForce false;
 
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/var/lib/sbctl";
-  };
+  # boot.lanzaboote = {
+  #   enable = true;
+  #   pkiBundle = "/var/lib/sbctl";
+  # };
 }
