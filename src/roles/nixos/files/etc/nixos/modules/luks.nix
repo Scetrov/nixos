5f942ff7bea67f51686a256b104d6b391d0a37d2
Richard Slater <git@scetrov.live>
2025-02-23 20:25:59 +0000

    fix(): give up on secure boot vendor disabled in UEFI

--- src/roles/nixos/files/etc/nixos/modules/luks.nix
+++ src/roles/nixos/files/etc/nixos/modules/luks.nix
@@ -9,17 +9,16 @@ in
   environment.systemPackages = [
     # For debugging and troubleshooting Secure Boot.
     pkgs.sbctl
-    pkgs.mokutil
   ];
 
   # Lanzaboote currently replaces the systemd-boot module.
   # This setting is usually set to true in configuration.nix
   # generated at installation time. So we force it to false
   # for now.
-  boot.loader.systemd-boot.enable = lib.mkForce false;
+  # boot.loader.systemd-boot.enable = lib.mkForce false;
 
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/var/lib/sbctl";
-  };
+  # boot.lanzaboote = {
+  #   enable = true;
+  #   pkiBundle = "/var/lib/sbctl";
+  # };
 }
