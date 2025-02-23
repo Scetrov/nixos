250e965815c76b618f52c42fccd783f2bd962c1f
Richard Slater <git@scetrov.live>
2025-02-23 15:54:41 +0000

    feat(): luks /w ansible

---
+++ src/roles/nixos/files/etc/nixos/modules/luks.nix
@@ -0,0 +1,25 @@
+# cite: https://laniakita.com/blog/d1fa986f/nixos-fde-tpm-hm-guide
+
+{ pkgs, lib, ... }:
+
+{
+  environment.systemPackages = [
+    luksCryptenroller
+    # For debugging and troubleshooting Secure Boot.
+    pkgs.sbctl
+    # Needed to use the TPM2 chip with `systemd-cryptenroll`
+    pkgs.tpm2-tss
+    pkgs.clevis
+  ];
+
+  # Lanzaboote currently replaces the systemd-boot module.
+  # This setting is usually set to true in configuration.nix
+  # generated at installation time. So we force it to false
+  # for now.
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/var/lib/sbctl";
+  };
+}
