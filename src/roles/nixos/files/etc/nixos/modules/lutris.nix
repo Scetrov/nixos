6d7f2909696aaff7511ea2b28b520ecaabf22b57
Richard Slater <git@scetrov.live>
2025-02-24 16:46:06 +0000

    feat(): lutris

---
+++ src/roles/nixos/files/etc/nixos/modules/lutris.nix
@@ -0,0 +1,9 @@
+{ config, ... }:
+let
+  baseconfig = { allowUnfree = true; };
+  unstable = import <nixos-unstable> { config = baseconfig; };
+in {
+  environment.systemPackages = with unstable; [
+    lutris
+  ];
+}
