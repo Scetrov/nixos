a28fb93840c1ddc412f1626161c8faeb5c075d84
Richard Slater <git@scetrov.live>
2025-02-28 17:29:16 +0000

    fix(): another typo

--- src/roles/nixos/files/etc/nixos/modules/user-scetrov.nix
+++ src/roles/nixos/files/etc/nixos/modules/user-scetrov.nix
@@ -6,7 +6,7 @@ let
   unstable = import <nixos-unstable> { config = baseconfig; };
 in
 {
-  age.secrets.scetrov-password.file = /root/secrets/scetrov_password.age;
+  age.secrets.scetrov-password.file = /root/secrets/scetrov_password.path;
   nix.settings.trusted-users = [
     "root"
     "scetrov"
