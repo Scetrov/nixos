a36782a61dfb0e35b76d1baea9ba312971f3b676
Richard Slater <git@scetrov.live>
2025-03-02 18:12:18 +0000

    feat(prometheus): Initial Commit

---
+++ src/roles/nixos/files/etc/nixos/modules/prometheus.nix
@@ -0,0 +1,41 @@
+{ config, lib, ... }:
+
+{
+  environment.etc.prometheus-config = {
+    source = ../../prometheus/prometheus.yml;
+    target = "prometheus/prometheus.yml";
+  };
+
+  services.prometheus = {
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9100;
+      };
+    };
+  };
+
+  virtualisation = {
+    oci-containers.containers = {
+      prometheus = {
+        image = "prom/prometheus";
+        autoStart = true;
+        volumes = [
+          "/etc/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml:ro"
+          "prometheus-data:/prometheus:rw"
+        ];
+        labels = { 
+          "traefik.enable" = "true";
+
+          # HTTPS RPC
+          "traefik.http.routers.prometheus.rule" = "Host(`metrics.net.scetrov.live`) || Host(`metrics-test.net.scetrov.live`)";
+          "traefik.http.routers.prometheus.tls" = "true";
+          "traefik.http.routers.prometheus.entrypoints" = "websecure";
+          "traefik.http.routers.prometheus.service" = "prometheus-service";
+          "traefik.http.services.prometheus-service.loadbalancer.server.port" = "9090";
+        };
+      };
+    };
+  };
+}
