2e055d95ab4a2ad8d372b2b63c9376ca7342a4eb
Richard Slater <git@scetrov.live>
2025-03-02 11:57:54 +0000

    fix(): auto-unlock kwallet

--- src/roles/nixos/files/etc/nixos/modules/xserver.nix
+++ src/roles/nixos/files/etc/nixos/modules/xserver.nix
@@ -9,8 +9,13 @@
   services.xrdp.defaultWindowManager = "startplasma-x11";
   services.xrdp.openFirewall = true;
 
+  security.pam.services.kwallet = {
+    name = "kwallet";
+    enableKwallet = true;
+  };
+
   programs.chromium.enablePlasmaBrowserIntegration = true;
-  
+
   services.xserver.xkb = {
     layout = "gb";
     variant = "";
