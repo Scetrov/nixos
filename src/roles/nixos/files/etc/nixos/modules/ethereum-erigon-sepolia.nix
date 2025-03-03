8f375fc802a1386e1175f9e884f34d21e8b7304e
Richard Slater <git@scetrov.live>
2025-03-03 15:02:13 +0000

    fix(): fix path

--- src/roles/nixos/files/etc/nixos/modules/ethereum-erigon-sepolia.nix
+++ src/roles/nixos/files/etc/nixos/modules/ethereum-erigon-sepolia.nix
@@ -22,7 +22,7 @@
     script = ''
       mkdir --parents "$ETHEREUM_DATA/erigon/sepolia"
       mkdir --parents "$ETHEREUM_DATA/erigon/sepolia/share"
-      chown --recursive erigon:ethereum "$ETHEREUM_DATA/erigon"
+      chown --recursive erigon:ethereum "$ETHEREUM_DATA"
       find "$ETHEREUM_DATA" -type d -exec chmod 750 {} +
       find "$ETHEREUM_DATA" -type f -exec chmod 640 {} +
     '';
