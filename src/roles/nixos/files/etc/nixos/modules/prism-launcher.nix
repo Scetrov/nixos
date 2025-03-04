96dc5f84d0f0b951d96b34eb3229361d22617fc4
Richard Slater <git@scetrov.live>
2025-03-04 20:12:04 +0000

    feat(minecraft): prisim launcher

---
+++ src/roles/nixos/files/etc/nixos/modules/prism-launcher.nix
@@ -0,0 +1,13 @@
+{ config, pkgs }:
+
+{
+  users.users.scetrov = {
+    packages = with pkgs; [
+      (prismlauncher.override {
+        jdks = [
+          zulu23
+        ];
+      })
+    ];
+  };
+}
