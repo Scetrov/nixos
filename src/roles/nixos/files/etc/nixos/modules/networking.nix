fdeb3676e070de829aced2ca368b61aae59b699b
Richard Slater <git@scetrov.live>
2025-03-05 15:44:32 +0000

    fix(): structure of networking

--- src/roles/nixos/files/etc/nixos/modules/networking.nix
+++ src/roles/nixos/files/etc/nixos/modules/networking.nix
@@ -1,29 +1,31 @@
 { config, ... }:
 
 {
-  networking.firewall.enable = true;
+  networking = {
+    firewall.enable = true;
 
-  hosts = {
-    "10.229.10.1" = [
-      "fyne"
-      "fyne.net.scetrov.live"
-    ];
-    "10.229.10.2" = [
-      "habiki"
-      "habiki.net.scetrov.live"
-      "traefik.net.scetrov.live"
-      "grafana.net.scetrov.live"
-      "prometheus.net.scetrov.live"
-      "json-rpc.sepolia.scetrov.live"
-      "json-rpc.pyrope.scetrov.live"
-    ];
-    "10.229.10.10" = [
-      "bullit"
-      "bullit.net.scetrov.live"
-    ];
-    "10.229.10.11" = [
-      "woodford"
-      "woodford.net.scetrov.live"
-    ];
+    hosts = {
+      "10.229.10.1" = [
+        "fyne"
+        "fyne.net.scetrov.live"
+      ];
+      "10.229.10.2" = [
+        "habiki"
+        "habiki.net.scetrov.live"
+        "traefik.net.scetrov.live"
+        "grafana.net.scetrov.live"
+        "prometheus.net.scetrov.live"
+        "json-rpc.sepolia.scetrov.live"
+        "json-rpc.pyrope.scetrov.live"
+      ];
+      "10.229.10.10" = [
+        "bullit"
+        "bullit.net.scetrov.live"
+      ];
+      "10.229.10.11" = [
+        "woodford"
+        "woodford.net.scetrov.live"
+      ];
+    };
   };
 }
