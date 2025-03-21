ff679886afbdc4401dd2490971d6f587f044e7d0
Scetrov <git@scetrov.live>
2025-03-21 15:29:10 +0000

    fix(): script

--- scripts/play.sh
+++ scripts/play.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-git add . && git commit && git push origin HEAD && ansible-playbook -i src/inventory.yml src/playbook.yml --vault-password-file ~/.ansible/nixos_vault_password
+git add . && git commit || git push origin HEAD && ansible-playbook -i src/inventory.yml src/playbook.yml --vault-password-file ~/.ansible/nixos_vault_password
