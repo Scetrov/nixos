e203dff4ca94d4683d1e2f4bdfe1ae7d9e7443f0
Scetrov <git@scetrov.live>
2025-03-23 15:45:52 +0000

    fix(): uraccetped

--- src/roles/nixos/files/etc/nixos/modules/user-scetrov.nix
+++ src/roles/nixos/files/etc/nixos/modules/user-scetrov.nix
@@ -61,7 +61,7 @@ in
     overrideFolders = true;
 
     openDefaultPorts = true;
-    
+
     settings = {
       devices = {
         "woodford" = { id = "BCXATW4-QDVK6DP-G42DTSY-R62SFE3-EKSGV4I-EAIPXE2-HUK7SOX-6KJP5A7"; };
@@ -75,7 +75,7 @@ in
         };
       };
       options = {
-        "urAccepted" = 1;
+        "urAccepted" = 99;
       };
       gui = {
         user = "scetrov";
