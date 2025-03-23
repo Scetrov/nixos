b64e9fb7583584f1bf81a4aa3392ffa6325660ae
Scetrov <git@scetrov.live>
2025-03-23 13:56:31 +0000

    fix(): temp disable syncthing

--- src/roles/nixos/files/etc/nixos/modules/user-scetrov.nix
+++ src/roles/nixos/files/etc/nixos/modules/user-scetrov.nix
@@ -50,7 +50,7 @@ in
   };
 
   services.syncthing = {
-    enable = true;
+    enable = false;
     user = "scetrov";
     dataDir = "/home/scetrov/.local/share/syncthing";
     configDir = "/home/scetrov/.config/syncthing";
