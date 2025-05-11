518269ba23e1a313590dc3ddf04c18d662de8590
Scetrov <git@scetrov.live>
2025-05-11 20:22:07 +0100

    feat(): blocky DNS filtering

---
+++ src/roles/nixos/files/etc/nixos/modules/blocky.nix
@@ -0,0 +1,47 @@
+{ config, ...}:
+
+{
+  services.blocky = {
+    enable = true;
+    settings = {
+      ports.dns = 53;
+      ports.http = 4000;
+      upstreams.groups.default = [
+        "https://one.one.one.one/dns-query" # Cloudflare
+        "https://dns.google/dns-query" # Google
+        "https://dns.quad9.net/dns-query" # Quad9
+      ];
+      bootstrapDns = {
+        upstream = "https://one.one.one.one/dns-query";
+        ips = [ "1.1.1.1" "1.0.0.1" ];
+      };
+      blocking = {
+        blackLists = {
+          abuse = ["https://blocklistproject.github.io/Lists/abuse.txt"];
+          drugs = ["https://blocklistproject.github.io/Lists/drugs.txt"];
+          fraud = ["https://blocklistproject.github.io/Lists/fraud.txt"];
+          gambling = ["https://blocklistproject.github.io/Lists/gambling.txt"];
+          malware = ["https://blocklistproject.github.io/Lists/malware.txt"];
+          phishing = ["https://blocklistproject.github.io/Lists/phishing.txt"];
+          piracy = ["https://blocklistproject.github.io/Lists/piracy.txt"];
+          porn = ["https://blocklistproject.github.io/Lists/porn.txt"];
+          scam = ["https://blocklistproject.github.io/Lists/scam.txt"];
+        };
+        clientGroupsBlock = {
+          default = [ "adult" "malware" "phishing" ];
+        };
+      };
+    };
+    caching = {
+      minTime = "5m";
+      maxTime = "30m";
+      prefetching = true;
+    };
+    prometheus = {
+      enable = true;
+    };
+    hostsFile = {
+      sources = [ "/etc/hosts" ];
+    };
+  };
+}
