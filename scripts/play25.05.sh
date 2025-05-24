40e888c1c580d18b992e7d98cd168b15218012fd
Scetrov <git@scetrov.live>
2025-05-24 22:06:17 +0100

    feat(): NixOS 25.05

--- scripts/play25.05.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-git add . && git commit && git push origin HEAD
-
-ansible-playbook -i src/inventory25.05.yml src/playbook.yml --vault-password-file ~/.ansible/nixos_vault_password
