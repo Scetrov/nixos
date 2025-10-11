ec965d261bc3cfedb5ba57499be02eb14de45379
Scetrov <git@scetrov.live>
2025-10-11 22:02:41 +0100

    fix: improve devcontainers experience

--- src/roles/nixos/files/etc/nixos/modules/user-scetrov-gui.nix
+++ src/roles/nixos/files/etc/nixos/modules/user-scetrov-gui.nix
@@ -4,10 +4,61 @@ let
     allowUnfree = true;
   };
   unstable = import <nixos-unstable> { config = baseconfig; };
+
+  vscode-fhs-devcontainers = pkgs.buildFHSUserEnv {
+    name = "vscode-fhs-devcontainers";
+    targetPkgs =
+      p: with p; [
+        unstable.vscode
+        podman # podman CLI
+        git
+        gnupg
+        cacert
+        bashInteractive
+      ];
+    runScript = "bash";
+    # expose both /var/run and /run/user for rootless sockets + your home
+    extraMounts = [
+      {
+        source = "/var/run";
+        target = "/var/run";
+      }
+      {
+        source = "/run/user";
+        target = "/run/user";
+      }
+      {
+        source = "/home/scetrov";
+        target = "/home/scetrov";
+      }
+    ];
+  };
+
 in
 {
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true; # VS Code Dev Containers talks to "docker" API
+  };
+
+  # Required for rootless mappings
+  users.users.scetrov.subUidRanges = [
+    {
+      startUid = 100001;
+      count = 65536;
+    }
+  ];
+  users.users.scetrov.subGidRanges = [
+    {
+      startGid = 100001;
+      count = 65536;
+    }
+  ];
+
   users.users.scetrov = {
     packages = with pkgs; [
+      unstable.vscode
+      vscode-fhs-devcontainers
       charles
       discord
       jetbrains.rider
@@ -20,7 +71,14 @@ in
       unstable.obsidian
       unstable.sqlitestudio
       unstable.syncthingtray
-      unstable.vscode-fhs
     ];
   };
+
+  environment.systemPackages = [ vscode-fhs-devcontainers ];
+  environment.etc."profile.d/code-fhs.sh".text = ''
+    code-fhs() {
+      exec ${vscode-fhs-devcontainers}/bin/vscode-fhs-devcontainers -c "exec code ''${*:-.}"
+    }
+    export -f code-fhs
+  '';
 }
