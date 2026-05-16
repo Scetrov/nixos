ee3082a39a33d1bddba27400741be472492e7844
Scetrov <git@scetrov.live>
2026-05-16 20:56:48 +0100

    feat: infrastructure as code foundation (opentofu)

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+variable "authentik_token" {
+  type      = string
+  sensitive = true
+}
+
+terraform {
+  backend "pg" {
+    # Pass via -backend-config="conn_str=..."
+  }
+
+  required_providers {
+    authentik = {
+      source = "goauthentik/authentik"
+      version = "2024.12.0"
+    }
+    caddy = {
+      source  = "conradludgate/caddy"
+      version = "0.2.8"
+    }
+  }
+}
+\provider "authentik" {
+  url   = "https://identity.net.scetrov.live"
+  token = var.authentik_token
+}
+
+provider "caddy" {
+  host = "http://10.229.10.2:2019"
+}
