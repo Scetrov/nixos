4067512a871286f580b8be10e73ad908849312f2
Scetrov <git@scetrov.live>
2026-06-13 19:38:06 +0100

    fix: xrdp

--- src/roles/nixos/files/etc/nixos/modules/xrdp.nix
+++ src/roles/nixos/files/etc/nixos/modules/xrdp.nix
@@ -1,5 +1,28 @@
 { config, pkgs, ... }:
 
+let
+  xrdpPlasmaSession = pkgs.writeShellScript "xrdp-startplasma-x11" ''
+    export XDG_SESSION_TYPE=x11
+    export DESKTOP_SESSION=plasma
+    export KDE_SESSION_VERSION=6
+
+    unset QT_SCREEN_SCALE_FACTORS
+    unset QT_SCALE_FACTOR
+    unset KSCREEN_BACKEND
+
+    export KWIN_COMPOSE=N
+
+    if command -v kwriteconfig6 >/dev/null 2>&1; then
+      kwriteconfig6 --file kdeglobals --group KScreen --key ScaleFactor --delete || true
+      kwriteconfig6 --file kdeglobals --group KScreen --key ScreenScaleFactors --delete || true
+      kwriteconfig6 --file kwinrc --group Compositing --key Enabled false || true
+    fi
+
+    rm -rf "$HOME/.local/share/kscreen"
+
+    exec ${pkgs.kdePackages.plasma-workspace}/bin/startplasma-x11
+  '';
+in
 {
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
@@ -8,7 +31,7 @@
   services.xrdp = {
     enable = true;
     openFirewall = true;
-    defaultWindowManager = "startplasma-x11";
+    defaultWindowManager = "${xrdpPlasmaSession}";
   };
 
   security.pam.services.kwallet = {
