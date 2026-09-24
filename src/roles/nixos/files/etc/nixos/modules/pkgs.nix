168e5b12e2c7212651a5476338bb5d1959a47842
Scetrov <git@scetrov.live>
2026-09-24 18:34:23 +0100

    chore: filter by arch

--- src/roles/nixos/files/etc/nixos/modules/pkgs.nix
+++ src/roles/nixos/files/etc/nixos/modules/pkgs.nix
@@ -8,22 +8,27 @@ in
 {
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-    ansible
-    bat
-    dig
-    gcc
-    go
-    niv
-    nixos-generators
-    nodejs_24
-    pnpm
-    steamcmd
-    networkmanager-openvpn
-    opentofu
-    openvpn
-    (python3.withPackages (ps: with ps; [ cryptography ]))
-    wget
-    (pkgs.callPackage <agenix/pkgs/agenix.nix> { })
-  ];
+  environment.systemPackages =
+    with pkgs;
+    [
+      ansible
+      bat
+      dig
+      gcc
+      go
+      niv
+      nixos-generators
+      nodejs_24
+      pnpm
+      networkmanager-openvpn
+      opentofu
+      openvpn
+      (python3.withPackages (ps: with ps; [ cryptography ]))
+      wget
+      (pkgs.callPackage <agenix/pkgs/agenix.nix> { })
+    ]
+    ++ pkgs.lib.optionals (pkgs.stdenv.hostPlatform.system == "x86_64-linux") [
+      # SteamCMD ships only for x86_64; evaluating it on Fyne (aarch64) fails.
+      steamcmd
+    ];
 }
